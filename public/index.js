4e0496b28954ce258576454a97d80a71b621ba2a
brtrduon <brianduongt@gmail.com>
2018-04-30 15:55:30 -0700

    cleaned stuff up. added points based on coordinates from places.json

--- public/index.js
+++ public/index.js
@@ -1,22 +1,20 @@
 d3.json('world110.json', (err, world) => {
     var countries = topojson.feature(world, world.objects.land);
-
     var width = 550
     var height = 550
     var projection = d3.geo.orthographic()
-    // var projection = d3.geo.albers()
-    // var projection = d3.geo.mercator()
         .scale(270)
         .rotate([100,0,0])
         .translate([width/2, height/2])
         .clipAngle(90);
 
     var path = d3.geo.path()
-        .projection(projection);
+        .projection(projection)
+        .pointRadius(2.5);
 
-    var svg = d3.select('svg')
+    var svg = d3.select('svg');
 
-    var graticule = d3.geo.graticule()
+    var graticule = d3.geo.graticule();
     svg.append("path")
         .datum(graticule)
         .attr("class", "graticule")
@@ -26,45 +24,28 @@ d3.json('world110.json', (err, world) => {
         .datum(countries)
         .attr("d", path)
         .classed("land", true);
-
-    var lonlat = [-78, 37.7];
-    var xy = projection(lonlat);
-
-    svg.append("circle")
-        .datum(lonlat)
-        .classed('point', true)
-        .attr({
-            cx: (d) => {
-                return projection(d)[0]
-            },
-            cy: (d) => {
-                return projection(d)[1]
-            },
-            r: 3,
-        })
     
-    navigator.geolocation.getCurrentPosition(function(pos){
-        console.log(pos);
-        var coords = [pos.coords.longitude, pos.coords.latitude];
-        var xy = projection(coords);
+    // navigator.geolocation.getCurrentPosition(function(pos){
+    //     console.log(pos);
+    //     var coords = [pos.coords.longitude, pos.coords.latitude];
+    //     var xy = projection(coords);
+    //     svg.append("circle")
+    //         .datum(coords)
+    //         .classed('myPoint', true)
+    //         .attr({
+    //             cx: (d) => {
+    //                 return projection(d)[0]
+    //             },
+    //             cy: (d) => {
+    //                 return projection(d)[1]
+    //             },
+    //             r: 3,
+    //         });
+    // });
 
-        svg.append("circle")
-            .datum(coords)
-            .classed('myPoint', true)
-            .attr({
-                cx: (d) => {
-                    return projection(d)[0]
-                },
-                cy: (d) => {
-                    return projection(d)[1]
-                },
-                r: 3,
-            })
-        })
-
+    // for rotating and zooming into the globe
     var zoom = d3.geo.zoom()
         .projection(projection)
-        // .scaleExtent([projection.scale() * 0.7, projection.scale() * 10])
         .on('zoom.redraw', () => {
             d3.event.sourceEvent.preventDefault();
             svg.selectAll('path').attr('d', path);
@@ -77,7 +58,24 @@ d3.json('world110.json', (err, world) => {
                     return projection(d)[1]
                 },
                 r: 3,
-            })
-        })
-        d3.selectAll('path').call(zoom);
+            });
+        });
+    d3.selectAll('path').call(zoom);
+
+    // load json containing data for locations' names, coordinates, etc
+    d3.json('places.json', (err, places) => {
+        svg.append('g').attr('class', 'points')
+            .selectAll('text').data(places.features)
+            .enter().append('path')
+            .attr('class', 'point')
+            .attr('d', path);
+
+        svg.append('g').attr('class', 'labels')
+            .selectAll('text').data(places.features)
+            .enter().append('text')
+            .attr('class', 'label')
+            .text((d) => {
+                return d.properties.name
+            });
+    });
 });
