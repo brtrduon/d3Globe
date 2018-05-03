e343984b319a462cbf8af1d31d7d551f01aa4060
brtrduon <brianduongt@gmail.com>
2018-05-03 14:56:03 -0700

    no errors, but i have no idea what im doing

--- public/charts.js
+++ public/charts.js
@@ -1,3 +1,62 @@
+// view/graph dimensions (?)
+
+var m = [20, 20, 30, 20];
+// I have no idea what the var above is for
+var width = 960 - m[1] - m[3];
+var height = 500 - m[0] - m[2];
+
+var x;
+var y;
+var duration = 1500;
+var delay = 500;
+
+var color = d3.scale.category10();
+
+var svg = d3.select('svg')
+    .attr('width', width + m[1] + m[3])
+    .attr('height', height + m[0] + m[2])
+    .append('g')
+        .attr('transform', `translate(${m[3]}, ${m[0]})`);
+
+// initialize stuff
+var bitcoin;
+var symbols;
+
+var line = d3.svg.line()
+    .interpolate('basis')
+    .x((d) => {
+        return x(d.time);
+        // might need to change 'time' for x axis if nothing is displaying on graph
+    })
+    .y((d) => {
+        return y(d.average);
+        // same thing as x axis
+    });
+
+var axis = d3.svg.line()
+    .interpolate('basis')
+    .x((d) => {
+        return x(d.date);
+    })
+    .y(height);
+    // not sure why I need this AND var line
+
+var area = d3.svg.area()
+    .interpolate('basis')
+    .x((d) => {
+        return x(d.time);
+    })
+    .y1((d) => {
+        return y(d.average);
+    });
+    // same thing here as the stuff above
+
+
+
+
+
+
+
 // recycling some stuff from globe.js to loop through currency used
 // will need to modify .js files once i make the whole shabangabang into a single page app
 
@@ -22,10 +81,22 @@ d3.json('https://blockchain.info/ticker', (err, data) => {
         result[k] = price[l];
     });
 
-    // console.log(result);
 
+    // need for loop here to be able to load all historical bitcoin data based on currencies being used
     for(var keys in result) {
-        console.log(`https://apiv2.bitcoinaverage.com/indices/global/history/BTC${keys}?period=alltime&?format=json`);
-        // need to d3.json pull from that api and draw it into that altering chart thing
-    }
+        d3.json(`https://apiv2.bitcoinaverage.com/indices/global/history/BTC${keys}?period=monthly&?format=json`, (err, data) => {
+            console.log(data);
+
+            var parse = d3.time.format('%b %Y').parse;
+            // i have no idea what the var above does
+
+            
+        });
+    };
+
+    // for(var m in realTime) {
+    //     d3.json(realTime[m], (err, data) => {
+    //         console.log(data);
+    //     });
+    // };
 });
